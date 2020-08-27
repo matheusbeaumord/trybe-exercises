ac5ecb9e047a714ec3227dd94fe539a14b2f5fe1
Matheus Beaumord <matheusbeaumord@gmail.com>
2020-08-27 19:25:44 -0300

    Envio exercice_8.js com rcorreção no if que estava errado

--- exercises/4_1/exercice_8.js
+++ exercises/4_1/exercice_8.js
@@ -1,8 +1,8 @@
 let a = 1;
-let b = 3;
+let b = 10;
 let c = 3;
 
-if ( a % a || b % b || c % b){
+if ( a % 2 == 0|| b % 2 == 0|| c % 2 == 0){
     console.log('true')
 } else {
     console.log('false')
