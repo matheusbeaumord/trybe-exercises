a8eaf4b89b6500e019d8c54a88a6dad37a342c26
Matheus Beaumord <matheusbeaumord@gmail.com>
2020-08-27 18:03:20 -0300

    Envio exercice_4.js

---
+++ exercises/4_1/exercice_4.js
@@ -0,0 +1,19 @@
+let a;
+a = 'negative';
+
+switch (a){
+    case 'positive':
+        console.log('positive');
+    break;
+
+    case 'negative':
+        console.log('negative');
+    break;
+
+    default:
+        console.log('zero');
+}
+
+
+
+// Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
