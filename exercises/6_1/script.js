15b22c455a99d5200a329c0e5e6b324408650666
Matheus Beaumord <matheusbeaumord@gmail.com>
2020-09-17 20:21:40 -0300

    Envio da tarefa do dia sem folha de estilo e com pendencia no requisito 3

---
+++ exercises/6_1/script.js
@@ -0,0 +1,37 @@
+var estados = [
+    'Acre',
+    'Alagoas',
+    'Amapá',
+    'Amazonas',
+    'Bahia',
+    'Ceará',
+    'Distrito Federal',
+    'Espírito Santo',
+    'Goiás',
+    'Maranhão',
+    'Mato Grosso',
+    'Mato Grosso do Sul',
+    'Minas Gerais',
+    'Pará',
+    'Paraíba',
+    'Paraná',
+    'Pernambuco',
+    'Piauí',
+    'Rio de Janeiro',
+    'Rio Grande do Norte',
+    'Rio Grande do Sul',
+    'Rondônia',
+    'Roraima',
+    'Santa Catarina',
+    'São Paulo',
+    'Sergipe',
+    'Tocantins'];
+
+
+estado = document.getElementById('estado')
+for (var i = 0; i < estados.length; i++){
+    var opt = document.createElement('option');
+    opt.value = i;
+    opt.innerHTML = estados[i];
+    estado.appendChild(opt);
+}
